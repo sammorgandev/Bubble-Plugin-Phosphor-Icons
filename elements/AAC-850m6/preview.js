4a931203112cc857a49821a88957bbc44e5f4015
Sam Morgan <154712411+sammorgandev@users.noreply.github.com>
2024-01-23 18:24:31 -0500

    Automatic commit from Bubble

---
+++ elements/AAC-850m6/preview.js
@@ -0,0 +1,30 @@
+function(instance, properties) {
+ // Retrieve the icon element created in the initialize function
+  var iconElement = document.createElement("i");
+  var existingLink = document.querySelector(`link[href*="@phosphor-icons"][href*="${properties.type}"]`);
+
+ // append the correct stylesheet to the header
+   	if (!existingLink) {
+    var dynamicStyle = properties.type; // Replace with the dynamic value as needed
+    var link = document.createElement('link');
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = 'https://unpkg.com/@phosphor-icons/web@2.0.3/src/' + dynamicStyle + '/style.css';
+    document.head.appendChild(link);
+    }
+    
+ // Update the class based on the selected icon type and style
+    //regular type has a unique class ".ph" instead of ."ph-regular" hence the if statement
+    if (properties.type == 'regular') { iconElement.className = "ph-" + properties.icon_id + " " + "ph" }
+    else { iconElement.className = "ph-" + properties.icon_id + " " + "ph-" + properties.type; }
+
+ // Apply additional styles like color and size
+    var iconWidth = properties.bubble.width()
+    iconElement.style.color = properties.color;
+    iconElement.style.fontSize = iconWidth + "px";
+    
+   
+ // Append the element to the canvas for display   
+    instance.canvas.append(iconElement);
+
+}
---
+++ elements/AAC-850m6/update.js
@@ -0,0 +1,43 @@
+function(instance, properties, context) {
+      let root = instance.canvas.get(0);
+
+ // Retrieve the icon element created in the initialize function
+    var iconElement = instance.data.iconElement;
+    
+ // Check to see if an existing stylesheet is already in the header since the update function runs often and this may not be the first time
+    var existingLink = document.querySelector(`link[href*="@phosphor-icons"][href*="${properties.type}"]`);
+
+ // append the correct stylesheet to the header if it doesn't already exist
+  	if (!existingLink) {
+   		var dynamicStyle = properties.type;
+    	var link = document.createElement('link');
+    	link.rel = "stylesheet";
+    	link.type = "text/css";
+    	link.href = 'https://unpkg.com/@phosphor-icons/web@2.0.3/src/' + dynamicStyle + '/style.css';
+    	document.head.appendChild(link);
+    }
+    
+    if (properties.clickable) {
+    root.style.cursor = "pointer";
+    root.addEventListener("click", instance.data.click);
+  	} else {
+    root.style.cursor = "inherit";
+    root.removeEventListener("click", instance.data.click);
+  	}
+    
+ // Update the class based on the selected icon type and style
+    //regular type has a unique class ".ph" instead of ."ph-regular" hence the if statement
+    if (properties.type == 'regular') { iconElement.className = "ph-" + properties.icon_id + " " + "ph" }
+    else { iconElement.className = "ph-" + properties.icon_id + " " + "ph-" + properties.type; }
+
+ // Apply additional styles like color and size
+    iconElement.style.color = properties.color;
+    iconElement.style.fontSize = properties.bubble.width() + "px"; // Assuming the size is in pixels
+    
+    
+ // Publish element states
+    instance.publishState('color', properties.color)
+    instance.publishState('type', properties.type)
+    instance.publishState('icon_id', properties.icon_id)
+    
+}
