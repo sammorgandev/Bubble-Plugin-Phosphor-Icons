9d13acdb2c9cf5df52dffe6ed2197c11b23b8c2a
Samuel Morgan <samuelmorgan@Samuels-MacBook-Pro.local>
2024-01-23 18:52:06 -0500

    jquery updates

--- elements/AAC-850m6/preview.js
+++ elements/AAC-850m6/preview.js
@@ -1,30 +1,32 @@
-function(instance, properties) {
- // Retrieve the icon element created in the initialize function
-  var iconElement = document.createElement("i");
-  var existingLink = document.querySelector(`link[href*="@phosphor-icons"][href*="${properties.type}"]`);
+function preview(instance, properties) {
+	// Retrieve the icon element created in the initialize function
+	var iconElement = document.createElement("i");
+	var existingLink = document.querySelector(
+		`link[href*="@phosphor-icons"][href*="${properties.type}"]`
+	);
 
- // append the correct stylesheet to the header
-   	if (!existingLink) {
-    var dynamicStyle = properties.type; // Replace with the dynamic value as needed
-    var link = document.createElement('link');
-    link.rel = "stylesheet";
-    link.type = "text/css";
-    link.href = 'https://unpkg.com/@phosphor-icons/web@2.0.3/src/' + dynamicStyle + '/style.css';
-    document.head.appendChild(link);
-    }
-    
- // Update the class based on the selected icon type and style
-    //regular type has a unique class ".ph" instead of ."ph-regular" hence the if statement
-    if (properties.type == 'regular') { iconElement.className = "ph-" + properties.icon_id + " " + "ph" }
-    else { iconElement.className = "ph-" + properties.icon_id + " " + "ph-" + properties.type; }
+	// append the correct stylesheet to the header
+	if (!existingLink) {
+		var dynamicStyle = properties.type;
+		const linkEl = $('<link rel="stylesheet" type="text/css" />');
+		linkEl.href = `https://unpkg.com/@phosphor-icons/web@2.0.3/src/${dynamicStyle}/style.css`; // this line is still good to have because it's user input and needs to be escaped properly
+		$("head").append(linkEl);
+	}
 
- // Apply additional styles like color and size
-    var iconWidth = properties.bubble.width()
-    iconElement.style.color = properties.color;
-    iconElement.style.fontSize = iconWidth + "px";
-    
-   
- // Append the element to the canvas for display   
-    instance.canvas.append(iconElement);
+	// Update the class based on the selected icon type and style
+	//regular type has a unique class ".ph" instead of ."ph-regular" hence the if statement
+	if (properties.type == "regular") {
+		iconElement.className = "ph-" + properties.icon_id + " " + "ph";
+	} else {
+		iconElement.className =
+			"ph-" + properties.icon_id + " " + "ph-" + properties.type;
+	}
 
+	// Apply additional styles like color and size
+	var iconWidth = properties.bubble.width();
+	iconElement.style.color = properties.color;
+	iconElement.style.fontSize = iconWidth + "px";
+
+	// Append the element to the canvas for display
+	instance.canvas.append(iconElement);
 }
