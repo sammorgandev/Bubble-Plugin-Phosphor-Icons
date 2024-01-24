0783f5c3c088c29eb5f62859509ebf450a3499c2
Sam Morgan <154712411+sammorgandev@users.noreply.github.com>
2024-01-23 19:13:41 -0500

    Automatic commit from Bubble

--- elements/AAC-850m6/initialize.js
+++ elements/AAC-850m6/initialize.js
@@ -1,21 +1,20 @@
-function initialize(instance, context) {
-	if (instance.data.iconElement) {
-		// Create an <i> tag for the icon
-		var iconElement =
-			instance.canvas.find("i")[0] || document.createElement("i");
-		instance.data.click = (event) => {
-			instance.triggerEvent("icon_is_clicked");
-			event.stopPropagation();
-		};
-		// Assign a default class
-		iconElement.className = "ph-plus-circle";
+function(instance, context) {
+// Create an <i> tag for the icon
+    var iconElement = instance.canvas.find('i')[0] || document.createElement("i");
+	instance.data.click = (event) => {
+    instance.triggerEvent("icon_is_clicked");
+    event.stopPropagation();
+  	};
+    // Assign a default class
+    iconElement.className = "ph-plus-circle"; 
+    
+    // Append the icon to the instance's DOM element if not already appended
+    if (!instance.canvas.find('i').length) {
+        instance.canvas.append(iconElement);
+    }
 
-		// Append the icon to the instance's DOM element if not already appended
-		if (!instance.canvas.find("i").length) {
-			instance.canvas.append(iconElement);
-		}
+    // Save the icon element in the instance for later use
+    instance.data.iconElement = iconElement;
 
-		// Save the icon element in the instance for later use
-		instance.data.iconElement = iconElement;
-	}
+  
 }
--- elements/AAC-850m6/preview.js
+++ elements/AAC-850m6/preview.js
@@ -1,26 +1,30 @@
-function preview(instance, properties) {
-	// Retrieve the icon element created in the initialize function
-	var iconElement = document.createElement("i");
-	var existingLink = document.querySelector(
-		`link[href*="@phosphor-icons"][href*="${properties.type}"]`
-	);
+function(instance, properties) {
+ // Retrieve the icon element created in the initialize function
+  var iconElement = document.createElement("i");
+  var existingLink = document.querySelector(`link[href*="@phosphor-icons"][href*="${properties.type}"]`);
 
-	// append the correct stylesheet to the header
-	if (!existingLink) {
-		var dynamicStyle = properties.type;
-		const linkEl = $('<link rel="stylesheet" type="text/css" />');
-		linkEl.href = `https://unpkg.com/@phosphor-icons/web@2.0.3/src/${dynamicStyle}/style.css`;
-	}
+ // append the correct stylesheet to the header
+   	if (!existingLink) {
+    var dynamicStyle = properties.type; // Replace with the dynamic value as needed
+    var link = document.createElement('link');
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = 'https://unpkg.com/@phosphor-icons/web@2.0.3/src/' + dynamicStyle + '/style.css';
+    document.head.appendChild(link);
+    }
+    
+ // Update the class based on the selected icon type and style
+    //regular type has a unique class ".ph" instead of ."ph-regular" hence the if statement
+    if (properties.type == 'regular') { iconElement.className = "ph-" + properties.icon_id + " " + "ph" }
+    else { iconElement.className = "ph-" + properties.icon_id + " " + "ph-" + properties.type; }
 
-	const typeClass =
-		properties.type === "regular" ? "ph" : `ph-${properties.type}`;
-	$(iconElement).addClass([`ph-${properties.icon_id}`, typeClass]);
+ // Apply additional styles like color and size
+    var iconWidth = properties.bubble.width()
+    iconElement.style.color = properties.color;
+    iconElement.style.fontSize = iconWidth + "px";
+    
+   
+ // Append the element to the canvas for display   
+    instance.canvas.append(iconElement);
 
-	// Apply additional styles like color and size
-	var iconWidth = properties.bubble.width();
-	iconElement.style.color = properties.color;
-	iconElement.style.fontSize = iconWidth + "px";
-
-	// Append the element to the canvas for display
-	instance.canvas.append(iconElement);
 }
