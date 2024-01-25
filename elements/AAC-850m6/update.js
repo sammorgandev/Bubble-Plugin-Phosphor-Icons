422a89a9364667b4cb1534739207de53ef34c244
Samuel Morgan <samuelmorgan@Samuels-MacBook-Pro.local>
2024-01-24 22:32:54 -0500

    bug fix -5

--- elements/AAC-850m6/update.js
+++ elements/AAC-850m6/update.js
@@ -17,7 +17,7 @@ function(instance, properties, context) {
     
     if (properties.clickable) {
     root.style.cursor = "pointer";
-    root.on("click", instance.data.click);
+    iconElement.on("click", instance.data.click);
   	} else {
     root.style.cursor = "inherit";
   	}
